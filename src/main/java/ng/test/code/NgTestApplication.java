8a072849b3259c12c9151691c6d8a5f1de040e7c
tusharp <sonalz@cdac.in>
2023-11-13 12:50:19 +0530

    first commit

---
+++ src/main/java/ng/test/code/NgTestApplication.java
@@ -0,0 +1,14 @@
+package ng.test.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgTestApplication.class, args);
+		System.out.println("----------Started--------");
+	}
+
+}
